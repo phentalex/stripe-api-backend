b245d381ba672b2cd6a6f3417fa035d0835993e0
Уваров Александр (phentalex) <phentalex@gmail.com>
2026-04-20 05:26:15 +0300

    Fix CSRF

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,6 +14,10 @@ DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
